6f6d4893530895e3f61a27e9f4f0688c0dbef922
venkat <venkat@facileconsulting.com>
2021-02-06 12:18:09 +0530

    flow

--- src/Login.js
+++ src/Login.js
@@ -1,19 +1,18 @@
 import React from 'react';
+// import { useHistory, browserHistory,
+//   Redirect } from "react-router-dom";
 class Login extends React.Component {
 handleClick()
 {
-  alert("click")
-  // window.location.href="/#/profile";
+  localStorage.token = "1";
+  // return <Redirect to="/profile" />;
+  window.location.href="/#/profile";
 }
-loadLogin()
-{
-  alert("load");
-  // if(localStorage.token) window.location.href="/#/profile";
-  
-}
+
   render() {
+    if(localStorage.token) {window.location.href="/#/profile"; return false;}
     return (
-      <div onLoad={ this.loadLogin } className="App">
+      <div className="App">
       <div className="wrapper">
         <div className="form-signin">
         <h2 className="form-signin-heading">Please login</h2>
--- src/Profile.js
+++ src/Profile.js
@@ -2,10 +2,19 @@
 import React from 'react';
 
 class Profile extends React.Component {
+  logout()
+  {
+    localStorage.token = "";
+    window.location.href="/#/login"; return false;
+  }
   render() {
+    if(!localStorage.token) {window.location.href="/#/login"; return false;}
     return (
       <div className="App">
-      Profile Component
+      <p>
+      Welcome <b>Saswati Mohanty</b>
+      </p>
+      <button onClick={this.logout}>Logout</button>
     </div>
     );
   }
